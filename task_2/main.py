39d6c580ccc6ebe1ef3cc639823d29f34648cbee
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-11-11 23:22:05 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -2,9 +2,12 @@
 
 import random
 
+numbers = [-3, -5, -2, -12, 0, 15, 4, 7, 2]
+
 rows = random.randint(4,8)
 colums = random.randint(4,8)
 
-matrix = [[0 for _ in range(rows)] for _ in range(colums)]
+matrix = [[random.choice(numbers) for _ in range(rows)] for _ in range(colums)]
 
 print(matrix)
+
