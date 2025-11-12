a5c959c403b0709d052229d7c2088fec7adeefcd
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-11-12 17:42:08 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -11,3 +11,12 @@ matrix = [[random.choice(numbers) for _ in range(rows)] for _ in range(colums)]
 
 for rows in matrix:
     print(" ".join(f"{element:>4}" for element in rows))
+
+sum_of_elements = 0
+
+for rows in matrix:
+    for element in rows:
+        if element % 3 == 0:
+            sum_of_elements += element
+
+print(f"Сумма элементов кратных 3: {sum_of_elements}")
