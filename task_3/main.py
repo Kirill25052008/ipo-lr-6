5ca1a30b84a40a0a014f26fe4b8f0cc135213eef
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-11-20 12:38:20 +0300

    Update main.py

--- task_3/main.py
+++ task_3/main.py
@@ -8,8 +8,25 @@ while a == False:
 
     if count_of_rows < 1:
         print("Ошибка!!!")
+        print("---------------------------------")
         continue
     else:
         break
 
 print(f"Введите {count_of_rows} стро(к/у/и)")
+
+i = 0
+list = []
+
+for _ in range(count_of_rows):
+    line = input(f"Введите строку № {i + 1} : ")
+    i += 1
+    list.append(line)
+
+text = "\n".join(list) # Объединяем строки в тнкст
+
+words = text.split() # Разбиваем текст на слова
+un_words = set(words) # Находим уникальные слова
+count = len(un_words) # Находим количество уникальных слов
+
+print(f"Количество уникальных слов = {count}")
